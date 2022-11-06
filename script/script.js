69f701adadc2d00de142e9f4df79f1f2b008e600
antonio murilo <107326926+MuriloAraujo174@users.noreply.github.com>
2022-11-06 17:25:18 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,8 @@
+let isIgnite = false
+
+function changeCard(event) {
+    const card = event.currentTarget;
+    const backgroundImage = isIgnite ?  "url(../assets/img-Explorer.svg)" : "url(../assets/img-Ignite.svg)"
+    isIgnite = !isIgnite
+    card.style.backgroundImage = backgroundImage  
+}
