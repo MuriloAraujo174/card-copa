d68046298d266054114d6e28b82819cdfafc8bb2
antonio murilo <107326926+MuriloAraujo174@users.noreply.github.com>
2022-11-06 17:41:09 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ let isIgnite = true
 
 function changeCard(event) {
     const card = event.currentTarget;
-    const backgroundImage = isIgnite ?  "url(../assets/img-Explorer.svg)" : "url(../assets/img-Ignite.svg)"
+    const backgroundImage = isIgnite ?  "url(./assets/img-Explorer.svg)" : "url(./assets/img-Ignite.svg)"
     isIgnite = !isIgnite
     card.style.backgroundImage = backgroundImage  
 }
